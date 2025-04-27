36df15197ca575e5f7e9d80d5e12766e8914b3c8
MiCetF <webmaster@micetf.fr>
2025-04-27 13:41:46 +0200

    UI: Amélioration de l'accessibilité et de l'esthétique
    - Optimisation des contrastes de couleurs pour conformité WCAG AA
    - Correction des popups pour éviter les retours à la ligne inesthétiques
    - Augmentation de la largeur des cartes (popup, record personnel, classement)
    - Ajout d'ombres de texte pour améliorer la lisibilité sur fonds colorés
    - Ajustement de la palette de couleurs pour une meilleure expérience visuelle
    - Amélioration du responsive design

--- index.php
+++ index.php
@@ -26,120 +26,110 @@ foreach (range(0, imagesx($img) - 1) as $x) {
     }
 }
 ?>
-<!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="utf-8">
-
-    <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-             More info: h5bp.com/i/378 -->
-<!--  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
-
-<title>Les rois de la souris (Parcours n°<?php echo $noCircuit; ?>)</title>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<meta name="description" content="Jeu d'entrainement à la manipulation de la souris. Parcours n°<?php echo $noCircuit; ?>" />
-<meta name="keywords" content="souris, psychomotricité fine, TUIC, B2I" />
-
-
-    <!-- Mobile viewport optimized: h5bp.com/viewport -->
-    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1">
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-
-    <link rel="stylesheet" href="css/circuit.css" type="text/css" media="screen" />
-
-    <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les rois de la souris (Parcours n°<?php echo $noCircuit; ?>)</title>
+    <meta name="description" content="Jeu d'entrainement à la manipulation de la souris. Parcours n°<?php echo $noCircuit; ?>" />
+    <meta name="keywords" content="souris, psychomotricité fine, TUIC, B2I" />
     <link rel="canonical" href="http://micetf.fr/Souris"/>
-
-    <!-- All JavaScript at the bottom, except this Modernizr build.
-             Modernizr enables HTML5 elements & feature detects for optimal performance.
-             Create your own custom Modernizr build: www.modernizr.com/download/ -->
+    
+    <!-- Styles et polices -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/circuit.css" type="text/css" media="screen" />
+    
+    <!-- Modernizr pour détection des fonctionnalités -->
     <script src="../library/js/modernizr.js"></script>
 </head>
 
 <body>
+    <!-- Section consignes -->
+    <div id="consignes">
+        <h1>Les rois de la souris</h1>
+        <div class="instructions">
+            <p>Toi aussi, tu veux devenir un roi de la souris ?</p>
+            <p>Une petite coccinelle se cache sous un rond vert. Elle ne sortira que si tu places le pointeur sur ce rond.</p>
+            <p>Elle remplacera alors le pointeur. En suivant le chemin bleu, tu dois amener cette petite coccinelle jusqu'au rond rouge.</p>
+            <p>Attention ! Si tu quittes le chemin bleu, tu as perdu.</p>
+            <p>Essaie d'aller vite !</p>
+        </div>
+        <a id="retour" class="btn-primary"><i class="fas fa-arrow-left"></i> Retour au jeu</a>
+    </div>
 
-<div id="consignes">
-<h1>Les rois de la souris</h1>
-<p>Toi aussi, tu veux devenir un roi de la souris ?</p>
-<br/>
-<p>Une petite coccinelle se cache sous un rond vert.</p>
-<p>Elle ne sortira que si tu places le pointeur sur ce rond.</p>
-<p>Elle remplacera alors le pointeur.</p>
-<p>En suivant le chemin bleu, tu dois amener cette petite coccinelle jusqu'au rond rouge.</p>
-<br/>
-<p>Attention ! Si tu quittes le chemin bleu, tu as perdu.</p>
-<p>Essaie d'aller vite !</p>
-<br/>
-<a id="retour">retour</a>
-</div>
-<div id="getPseudo">
-<h1>Les rois de la souris</h1>
-<h2>Saisis ton pseudo !</h2>
+    <!-- Section saisie pseudo -->
+    <div id="getPseudo">
+        <h1>Les rois de la souris</h1>
+        <h2>Saisis ton pseudo !</h2>
+        <h2>
+            <input type="text" value="Anonyme"/> 
+            <input type="button" value="OK" class="btn-primary"/>
+        </h2>
+    </div>
 
-<h2><input type="text" value="Anonyme"/> <input type="button" value="OK"/></h2>
-<br/>
-</div>
+    <!-- Section principale du jeu -->
+    <div id="jsOK">
+        <div id="popup">
+            <br/>
+            <h1></h1>
+            <p><input type="button" value="Recommencer" class="btn-primary"/></p>
+        </div>
+        
+        <div id="circuit"></div>
+        
+        <p id="chrono">0</p>
+        
+        <p id="personal-record" style="display:none;"></p>
+        
+        <p id="menu">
+            | <a id="aide" href="#"><i class="fas fa-question-circle"></i> Aide</a> |
+            
+            <?php foreach (range(1, $nbCircuits) as $i): ?>
+                <a class="changer <?php echo ($noCircuit == $i) ? 'parcours' : ''; ?>" 
+                   href="?c=<?php echo $i; ?>">
+                    <?php echo $i; ?>
+                </a> |
+            <?php endforeach; ?>
+        </p>
+        
+        <p id="pseudo" title="Changer de pseudo">[Anonyme]</p>
+        
+        <div id="records">
+            <p><i class="fas fa-trophy"></i> Top 10 des records pour le circuit n°<?php echo $noCircuit; ?></p>
+            <ol>
+                <li></li>
+            </ol>
+            <ul>
+                <li>Il n'y a aucun record pour ce circuit.</li>
+            </ul>
+        </div>
+    </div>
 
-<div id="jsOK">
-<div id="popup"><br/><h1></h1><p><input type="button" value="Recommencer"/></p></div>
-<div id="circuit"></div>
-<p id="chrono">0</p>
-<p id="personal-record" style="display:none;"></p>
-<p id="menu">
-| <a id="aide">Aide</a> |
-<?php
-foreach (range(1, $nbCircuits) as $i) {
-    ?>
- | <a class="changer <?php if ($noCircuit == $i) {
-     echo 'parcours';
- } ?>"
-        href="?c=<?php echo $i; ?>">
-            <?php echo $i; ?>
-     </a>
-<?php
-}
-?>
- |
-</p>
-<p id="pseudo" title="Changer de pseudo">[Anonyme]</p>
-<div id="records">
-<p>Top 10 des records pour le circuit n°<?php echo $noCircuit; ?></p>
-<ol>
-<li></li>
-</ol>
-<ul>
-<li>Il n'y a aucun record pour ce circuit.</li>
-</ul>
-</div>
-</div>
+    <!-- Message d'erreur si JavaScript désactivé -->
+    <div id="jsKO">
+        <h1>Vous devez activer le Javascript !</h1>
+    </div>
 
-<div id="jsKO"><h1>Vous devez activer le Javascript !</h1></div>
-<p>
-Créé par
-<a href="http://www.micetf.fr" title="Accueil">MiCetF</a> (2011) -
-<a id="contact" href="" title="Pour contacter le webmaster">contact</a>
-</p>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="ZXVEXH5392YTY">
-<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
-<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-</form>
+    <p>
+        Créé par <a href="http://www.micetf.fr" title="Accueil">MiCetF</a> (2011) -
+        <a id="contact" href="#" title="Pour contacter le webmaster">contact</a>
+    </p>
+    
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="ZXVEXH5392YTY">
+        <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+        <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+    </form>
 
-
-    <!-- scripts concatenated and minified via build script -->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"
-        integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0="
-        crossorigin="anonymous"></script>
-        <script type="text/javascript" src="../library/js/jquery.ui.touch-punch.min.js"></script>
-        <script type="text/javascript" src="../library/js/jquery-contact.min.js"></script>
-        <script type="text/javascript">
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
+    <script src="../library/js/jquery.ui.touch-punch.min.js"></script>
+    <script src="../library/js/jquery-contact.min.js"></script>
+    <script type="text/javascript">
         var parcours = '<?php echo $nomCircuit; ?>',
             pilote = '<?php echo $pseudo; ?>',
             sessionToken = '<?php echo $sessionToken; ?>',
@@ -149,8 +139,7 @@ Créé par
             echo "zBitmap[$x]=new Array(".implode(',', $ys).");";
         }
 ?>
-        </script>
-        <script type="text/javascript" src="js/circuit.min.js"></script>
-
+    </script>
+    <script src="js/circuit.min.js"></script>
 </body>
 </html>
